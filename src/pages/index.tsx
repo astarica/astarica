34d9c44623bcb39c9c93f15a27f5c5692f60c14c
Muhammad Wafa <wafax.4@gmail.com>
2021-06-10 07:03:00 +0700

    login page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import { Box } from "@chakra-ui/layout"
+import { Box, Divider, Heading, VStack } from "@chakra-ui/layout"
+
+import { Button } from "@chakra-ui/button"
 import { Container } from "../components/Container"
+import { Input } from "@chakra-ui/input"
 
 const Index = () => (
-  <Container height="100vh">
-    <Box>Hello World!</Box>
+  <Container justifyContent="center" height="100vh">
+    <Heading>Astarica</Heading>
+    <Box>Dashboard</Box>
+    <VStack my={6} as="form" w="full" maxW="xs" spacing={3}>
+      <Input variant="flushed" placeholder="username" type="text" />
+      <Input variant="flushed" placeholder="password" type="password" />
+      <Button colorScheme="teal" w="full">
+        Login
+      </Button>
+      <Divider />
+      <Box fontSize="small">Copyright &copy; {new Date().getFullYear()}</Box>
+    </VStack>
   </Container>
 )
 
