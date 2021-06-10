41eb37998deb9f666492652df8d9ef14d97d0c48
Muhammad Wafa <wafax.4@gmail.com>
2021-06-10 11:22:15 +0700

    link for test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,30 @@
 import { Box, Divider, Flex, HStack } from "@chakra-ui/layout"
 
 import { Button } from "@chakra-ui/button"
+import { useRouter } from "next/dist/client/router"
 
-export const Navbar = () => (
-  <Flex
-    justifyContent="flex-end"
-    bgColor="teal"
-    color="white"
-    p={4}
-    w="full"
-    mb={4}
-  >
-    <HStack>
-      <Box fontWeight="bold">Muhmmad Wafa</Box>
-      <Divider variant="solid" orientation="vertical" />
-      <Button size="sm" colorScheme="whiteAlpha">
-        Logout
-      </Button>
-    </HStack>
-  </Flex>
-)
+export const Navbar = () => {
+  const router = useRouter()
+  return (
+    <Flex
+      justifyContent="flex-end"
+      bgColor="teal"
+      color="white"
+      p={4}
+      w="full"
+      mb={4}
+    >
+      <HStack>
+        <Box fontWeight="bold">Muhmmad Wafa</Box>
+        <Divider variant="solid" orientation="vertical" />
+        <Button
+          size="sm"
+          colorScheme="whiteAlpha"
+          onClick={() => router.push("/")}
+        >
+          Logout
+        </Button>
+      </HStack>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,29 @@ import { Box, Divider, Heading, VStack } from "@chakra-ui/layout"
 import { Button } from "@chakra-ui/button"
 import { Container } from "../components/Container"
 import { Input } from "@chakra-ui/input"
+import { useRouter } from "next/dist/client/router"
 
-const Index = () => (
-  <Container justifyContent="center" height="100vh">
-    <Heading>Astarica</Heading>
-    <Box>Dashboard</Box>
-    <VStack my={6} as="form" w="full" maxW="xs" spacing={3}>
-      <Input variant="flushed" placeholder="username" type="text" />
-      <Input variant="flushed" placeholder="password" type="password" />
-      <Button colorScheme="teal" w="full">
-        Login
-      </Button>
-      <Divider />
-      <Box fontSize="small">Copyright &copy; {new Date().getFullYear()}</Box>
-    </VStack>
-  </Container>
-)
+const Index = () => {
+  const router = useRouter()
+  return (
+    <Container justifyContent="center" height="100vh">
+      <Heading>Astarica</Heading>
+      <Box>Dashboard</Box>
+      <VStack my={6} as="form" w="full" maxW="xs" spacing={3}>
+        <Input variant="flushed" placeholder="username" type="text" />
+        <Input variant="flushed" placeholder="password" type="password" />
+        <Button
+          colorScheme="teal"
+          w="full"
+          onClick={() => router.push("/dashboard")}
+        >
+          Login
+        </Button>
+        <Divider />
+        <Box fontSize="small">Copyright &copy; {new Date().getFullYear()}</Box>
+      </VStack>
+    </Container>
+  )
+}
 
 export default Index
