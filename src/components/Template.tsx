52ae3e6410e1b3d3118c84d9bac31727f739b1f0
Muhammad Wafa <wafax.4@gmail.com>
2021-06-10 10:47:28 +0700

    layout template broadcast

---
+++ src/components/Template.tsx
@@ -0,0 +1,20 @@
+import { Box, Heading, VStack } from "@chakra-ui/layout"
+
+import { Button } from "@chakra-ui/button"
+
+export const Template = () => (
+  <VStack p={4} alignItems="flex-start" w="full">
+    <Heading size="sm">Setting Template Undangan</Heading>
+    <Box borderLeftWidth={6} borderColor="teal" p={4} bgColor="white">
+      <Box mb={3}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab totam
+        dolores deserunt fugit illum debitis, ad ducimus tempore illo? Quas
+        dolorem, quisquam quis repellendus blanditiis tempore voluptatem eveniet
+        earum error porro possimus sequi quasi saepe delectus optio facere a?
+      </Box>
+      <Button size="xs" px={4} colorScheme="teal">
+        Edit
+      </Button>
+    </Box>
+  </VStack>
+)
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,13 +2,15 @@ import { Container } from "../components/Container"
 import { Hero } from "../components/Hero"
 import { Navbar } from "../components/Navbar"
 import { Stat } from "../components/Stat"
+import { Template } from "../components/Template"
 
 const Dashboard = () => {
   return (
-    <Container minH="100vh">
+    <Container minH="100vh" bgColor="gray.100">
       <Navbar />
       <Hero />
       <Stat />
+      <Template />
     </Container>
   )
 }
