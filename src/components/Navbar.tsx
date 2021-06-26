e19ff44f41901e9a26e622d73b74999cfa8ab195
Muhammad Wafa <wafax.4@gmail.com>
2021-06-26 14:31:49 +0700

    working logout button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,21 @@ import { Box, Divider, Flex, HStack } from "@chakra-ui/layout"
 
 import { Button } from "@chakra-ui/button"
 import { MeCtx } from "./MeCtx"
+import { fetcher } from "../services/fetcher"
 import { useContext } from "react"
 import { useRouter } from "next/dist/client/router"
 
 export const Navbar = () => {
   const router = useRouter()
   const { me } = useContext(MeCtx)
+
+  const logout = () => {
+    fetcher("/api/auth", { method: "DELETE" })
+      .catch(console.log)
+      .finally(() => {
+        router.push("/")
+      })
+  }
   return (
     <Flex
       justifyContent="flex-end"
@@ -20,11 +29,7 @@ export const Navbar = () => {
       <HStack>
         <Box fontWeight="bold">{me.name}</Box>
         <Divider variant="solid" orientation="vertical" />
-        <Button
-          size="sm"
-          colorScheme="whiteAlpha"
-          onClick={() => router.push("/")}
-        >
+        <Button size="sm" colorScheme="whiteAlpha" onClick={logout}>
           Logout
         </Button>
       </HStack>
