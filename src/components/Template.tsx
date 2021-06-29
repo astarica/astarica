3a70d050f1b946f46ac421f46ca9424fc4165528
Muhammad Wafa <wafax.4@gmail.com>
2021-06-30 00:37:14 +0700

    working update template

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,11 +1,21 @@
 import { Box, Heading, VStack } from "@chakra-ui/layout"
+import { Textarea, useToast } from "@chakra-ui/react"
+import { useContext, useEffect, useState } from "react"
 
 import { Button } from "@chakra-ui/button"
 import { MeCtx } from "./MeCtx"
-import { useContext } from "react"
+import { fetcher } from "../services/fetcher"
 
 export const Template = () => {
-  const { me } = useContext(MeCtx)
+  const { me, update } = useContext(MeCtx)
+  const [isEdit, setIsEdit] = useState(false)
+
+  const [template, setTemplate] = useState(me.template)
+  const toast = useToast()
+
+  useEffect(() => {
+    setTemplate(me.template)
+  }, [me])
 
   const templateFormatter = (t: string) => {
     return t
@@ -13,6 +23,19 @@ export const Template = () => {
       .replace(/(\*)([^\*]*)(\*)/g, "<strong>$2</strong>")
       .replace(/(\_)([^\_]*)(\_)/g, "<i>$2</i>")
       .replace(/(\~)([^\~]*)(\~)/g, "<s>$2</s>")
+  }
+
+  const updateTemplate = () => {
+    fetcher<{ error: boolean }>("/api/template", {
+      method: "POST",
+      body: JSON.stringify({ template }),
+    })
+      .then((d) => {
+        if (!d.error)
+          toast({ title: "Template telah di update.", status: "success" })
+      })
+      .catch(console.log)
+      .finally(() => update())
   }
 
   return (
@@ -27,15 +50,31 @@ export const Template = () => {
         p={4}
         bgColor="white"
       >
-        <Box
-          w="full"
-          mb={3}
-          dangerouslySetInnerHTML={{
-            __html: templateFormatter(me.template) || "",
+        {isEdit ? (
+          <Textarea
+            value={template}
+            onChange={({ target }) => setTemplate(target.value)}
+            rows={template.split("\n").length}
+          />
+        ) : (
+          <Box
+            w="full"
+            mb={3}
+            dangerouslySetInnerHTML={{
+              __html: templateFormatter(me.template) || "",
+            }}
+          ></Box>
+        )}
+        <Button
+          size="xs"
+          px={4}
+          colorScheme="teal"
+          onClick={() => {
+            if (isEdit) updateTemplate()
+            setIsEdit((v) => !v)
           }}
-        ></Box>
-        <Button size="xs" px={4} colorScheme="teal">
-          Edit
+        >
+          {isEdit ? "Submit" : "Edit"}
         </Button>
       </Box>
     </VStack>
