8de7c107321404ff7a7fc9b24aedb0f6cad1186e
Muhammad Wafa <wafax.4@gmail.com>
2021-07-01 12:53:45 +0700

    add tabs on setting #2

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,5 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
+
 import { Comment } from "../components/Comment"
 import { Container } from "../components/Container"
 import Head from "next/head"
@@ -18,9 +20,24 @@ const Dashboard = () => {
         <Navbar />
         <Hero />
         <Stat />
-        <Template />
-        <Invitation />
-        <Comment />
+        <Tabs variant="solid-rounded" w="full" colorScheme="teal">
+          <TabList p={4}>
+            <Tab>Setting Template</Tab>
+            <Tab>Undangan</Tab>
+            <Tab>Ucapan</Tab>
+          </TabList>
+          <TabPanels bg="gray.200">
+            <TabPanel>
+              <Template />
+            </TabPanel>
+            <TabPanel>
+              <Invitation />
+            </TabPanel>
+            <TabPanel>
+              <Comment />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Container>
     </MeProvider>
   )
